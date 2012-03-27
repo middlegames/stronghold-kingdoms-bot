9f8892fd26c5fe969ed66348f4e02f1025e0c322
Middle Gamer <middlegamer@gmail.com>
2012-03-27 22:09:57 +0200

    Build correct classpath outside of mingw, add log file

--- shkbot.bat
+++ shkbot.bat
@@ -1,1 +1,18 @@
-java -Xmx512m -classpath "lib/sikuli-script.jar;bin" -splash:"data\images\splash-screen-final.png" com.middlegames.shkbot.gui.BotUI
+@echo off
+
+SET LOG=shkbot.log
+GOTO :BuildClasspath
+
+:BuildClasspath
+FOR /R ./lib %%a in (*.jar) DO CALL :AddToPath %%a
+SET CLASSPATH=%1;%CLASSPATH%bin
+GOTO :StartBot
+
+:AddToPath
+SET CLASSPATH=%1;%CLASSPATH%
+GOTO :EOF
+
+:StartBot
+echo Logging shkbot output to %LOG% 
+echo Classpath %CLASSPATH% > %LOG%
+java -Xmx512m -classpath %CLASSPATH% -splash:"data\images\splash-screen-final.png" com.middlegames.shkbot.gui.BotUI >> %LOG%
