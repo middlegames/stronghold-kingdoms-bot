e8045f331ed742b806be0ff595be9416508141c1
Middle Gamer <middlegamer@gmail.com>
2012-03-27 22:47:36 +0200

    Create binaries if missing, data struct checks

--- shkbot.bat
+++ shkbot.bat
@@ -1,6 +1,15 @@
 @echo off
 
-SET LOG=shkbot.log
+IF NOT EXIST data\village (
+    echo Error: data\village directory is missing
+    GOTO :EOF
+)
+IF NOT EXIST data\parish (
+    echo Error: data\parish directory is missing
+    GOTO :EOF
+)
+
+SET LOG=bot.log
 GOTO :BuildClasspath
 
 :BuildClasspath
@@ -12,7 +21,15 @@ GOTO :StartBot
 SET CLASSPATH=%1;%CLASSPATH%
 GOTO :EOF
 
+:Compile
+echo Compiling shkbot sources...
+md bin
+%JAVA_HOME%\bin\javac -classpath %CLASSPATH% -d bin -source 7 -sourcepath src\main\java src\main\java\com\middlegames\shkbot\gui\BotUI.java
+echo Compiled
+GOTO :StartBot
+
 :StartBot
+IF NOT EXIST bin GOTO Compile
 echo Logging shkbot output to %LOG% 
 echo Classpath %CLASSPATH% > %LOG%
-java -Xmx512m -classpath %CLASSPATH% -splash:"data\images\splash-screen-final.png" com.middlegames.shkbot.gui.BotUI >> %LOG%
+java -Xmx512m -classpath "%CLASSPATH%" -splash:"data\images\splash-screen-final.png" com.middlegames.shkbot.gui.BotUI >> %LOG%
