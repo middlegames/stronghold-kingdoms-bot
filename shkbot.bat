39a9d78ae1b831ad01ce4b6bc3e8c223a99e6cd9
Middle Gamer <middlegamer@gmail.com>
2012-04-10 21:14:41 +0200

    Tooltips note and verbose compilation mode
    refs #5
    refs #6

--- shkbot.bat
+++ shkbot.bat
@@ -24,7 +24,7 @@ GOTO :EOF
 :Compile
 echo Compiling shkbot sources...
 md bin
-%JAVA_HOME%\bin\javac -classpath %CLASSPATH% -d bin -source 7 -sourcepath src\main\java src\main\java\com\middlegames\shkbot\gui\BotUI.java
+%JAVA_HOME%\bin\javac -classpath %CLASSPATH% -d bin -source 7 -sourcepath src\main\java src\main\java\com\middlegames\shkbot\gui\BotUI.java -verbose 1>&2 2> compilation.log
 echo Compiled
 GOTO :StartBot
 
